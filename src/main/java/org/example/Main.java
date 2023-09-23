da6c7f1ef461a5f69d8b65d6781d59d2f2c268c5
KateNik <nikitinaekaterina927@gmail.com>
2023-09-24 00:50:29 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,108 @@
+package org.example;
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import javafx.scene.Node;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+import javax.imageio.metadata.IIOMetadataNode;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static jdk.xml.internal.JdkXmlUtils.getValue;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        String jsonFilename = "data.json";
+        writeString(json, jsonFilename);
+
+        String XmlFileName = "data.xml";
+        List<Employee> list2 = parseXML(XmlFileName);
+       writeString(json, "data2.json");
+
+    }
+
+    public static List<Employee> parseXML(String xmlFilename) {
+
+        List<Employee> list = new ArrayList<>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new File(xmlFilename));
+            NodeList nodeList = document.getDocumentElement().getChildNodes();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (Node.ELEMENT_NODE == node.getNodeType()) {
+                    Element element = (Element) node;
+                    long id = Long.parseLong(getValue(element, "id"));
+                    String firstName = getValue(element, "firstName");
+                    String lastName = getValue(element, "lastName");
+                    String country = getValue(element, "country");
+                    int age = Integer.parseInt(getValue(element, "age"));
+                    Employee employee = new Employee(id, firstName, lastName, country, age);
+                    list.add(employee);
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+
+    public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<Employee>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            return csv.parse();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return List.of();
+    }
+
+    public static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.setPrettyPrinting().create();
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        return gson.toJson(list, listType);
+    }
+
+    public static void writeString(String json, String jsonFilename) {
+        try (FileWriter fileWriter = new FileWriter(jsonFilename)) {
+            fileWriter.write(json);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
